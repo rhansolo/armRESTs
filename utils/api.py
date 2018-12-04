3608c59bd70a8e5dd7504f48802022cb23018e62
jwongUlti25 <jwong25@stuy.edu>
2018-12-04 13:57:37 -0500

    project in compliance with key.txt requirements

--- utils/api.py
+++ utils/api.py
@@ -7,7 +7,23 @@ import json
 import random
 import requests
 import urllib.request as request
+
 from urllib.request import Request
+
+'''
+LIST OF KEYS
+'''
+key0 = open('utils/key0.txt', 'r')
+key1 = open('utils/key1.txt', 'r')
+key2 = open('utils/key2.txt', 'r')
+movieDB_key = key0.readline().strip()
+showtimes_key = key1.readline().strip()
+ipStack_key = key2.readline().strip()
+key0.close()
+key1.close()
+key2.close()
+
+
 '''
 API HELPERS
 '''
@@ -33,8 +49,7 @@ examples: https://www.themoviedb.org/documentation/api/discover
 def getMovieDict(id):
     ''' Getting entire associated movie dictionary '''
     url = "https://api.themoviedb.org/3/movie/{0}?api_key={1}"
-    key = "95e69e7f8882e106d7cf82de25f6a422"
-    return fetchInfo(url.format(str(id), key))
+    return fetchInfo(url.format(str(id), movieDB_key))
 
 def getMovieName(id):
     dict= getMovieDict(id)
@@ -42,16 +57,14 @@ def getMovieName(id):
 
 def getPopular():
     ''' Getting trending movies '''
-    movURL=" https://api.themoviedb.org/3/movie/popular?api_key="
-    movKey= "95e69e7f8882e106d7cf82de25f6a422"
-    movLang="&language=en-US&page=1"
-    movInfo= fetchInfo(movURL+movKey+movLang)
+    movURL=" https://api.themoviedb.org/3/movie/popular?api_key={0}&language=en-US&page=1"
+    movInfo= fetchInfo(movURL.format(movieDB_key))
     return movInfo['results'];
 
 def getGenres():
     ''' Getting all possible genres '''
-    genURL="https://api.themoviedb.org/3/genre/movie/list?api_key=95e69e7f8882e106d7cf82de25f6a422&language=en-US"
-    genInfo= fetchInfo(genURL)
+    genURL="https://api.themoviedb.org/3/genre/movie/list?api_key={0}&language=en-US"
+    genInfo= fetchInfo(genURL.format(movieDB_key))
     return genInfo['genres']
 
 GENRES = getGenres()
@@ -60,7 +73,7 @@ def getMovies(genre):
     ''' Getting all movies given genre '''
     search_for_genre = [x for x in GENRES if x['name'] == genre][0]
     genre_id = search_for_genre['id']
-    genURL = "https://api.themoviedb.org/3/discover/movie?api_key=95e69e7f8882e106d7cf82de25f6a422&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres={0}".format(genre_id)
+    genURL = "https://api.themoviedb.org/3/discover/movie?api_key={0}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres={1}".format(movieDB_key,genre_id)
     genInfo = fetchInfo(genURL)
     return genInfo['results']
 
@@ -68,28 +81,28 @@ def searchMovie(keyword, page = '1'):
     '''Getting all movies given keyword'''
     word = keyword
     # fix space issue in keyword
-    keywordURL = "https://api.themoviedb.org/3/search/movie?api_key=95e69e7f8882e106d7cf82de25f6a422&language=en-US&query={0}&page={1}&include_adult=false".format(word.replace(" ", "%20"), page)
+    keywordURL = "https://api.themoviedb.org/3/search/movie?api_key={0}&language=en-US&query={1}&page={2}&include_adult=false".format(movieDB_key, word.replace(" ", "%20"), page)
     return fetchInfo(keywordURL)['results']
 
 def getRandom():
     year = random.randint(2000,2018)
     num = random.randint(0,19)
-    movURL = "https://api.themoviedb.org/3/discover/movie?api_key=95e69e7f8882e106d7cf82de25f6a422&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&year=" + str(year)
+    movURL = "https://api.themoviedb.org/3/discover/movie?api_key={0}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&year={1}".format(movieDB_key, str(year))
     page = random.randint(1,8)
-    newMovUrl = "https://api.themoviedb.org/3/discover/movie?api_key=95e69e7f8882e106d7cf82de25f6a422&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=" + str(page) + "&year=" + str(year)
+    newMovUrl = "https://api.themoviedb.org/3/discover/movie?api_key={0}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=".format(movieDB_key) + str(page) + "&year=" + str(year)
     id = fetchInfo(newMovUrl)['results'][num]['id']
     return getMovieDict(str(id))
 
 def getTrailer(id):
     movURL = "https://api.themoviedb.org/3/movie/"
-    movRest ="/videos?api_key=95e69e7f8882e106d7cf82de25f6a422&language=en-US"
+    movRest ="/videos?api_key={0}&language=en-US".format(movieDB_key)
     genInfo = fetchInfo(movURL + str(id) + movRest)
     key = genInfo['results'][0]['key']
     link =  "https://www.youtube.com/embed/" + str(key)
     return link
 def getReviews(id):
     movURL =  "https://api.themoviedb.org/3/movie/"
-    movRest = "/reviews?api_key=95e69e7f8882e106d7cf82de25f6a422&language=en-US&page=1"
+    movRest = "/reviews?api_key={0}&language=en-US&page=1".format(movieDB_key)
     genInfo = fetchInfo(movURL + str(id) + movRest)
     return genInfo['results']
 
@@ -114,7 +127,7 @@ def send_request():
                 "distance": "30",
             },
             headers={
-                "X-API-Key": "h54sMq1Q8UinW1K91Ts3fxPJ34CYMQAC",
+                "X-API-Key": "{0}".format(showtimes_key),
             },
         )
         print('Response HTTP Status Code: {status_code}'.format(
@@ -137,25 +150,24 @@ documentation: https://ipstack.com/quickstart
 https://ipstack.com/documentation
 '''
 ipStackURL= "http://api.ipstack.com/"
-KEY= "5eccde7ed4a5f5de86b6c688b239d7c2"
 
 def getZip(IP):
     try:
-        locInfo= fetchInfo(ipStackURL+IP+"?access_key="+KEY)
+        locInfo= fetchInfo(ipStackURL+IP+"?access_key="+ipStack_key)
         return locInfo["zip"]
     except:
         print("no zip")
 
 def getLat(IP):
     try:
-        locInfo= fetchInfo(ipStackURL+IP+"?access_key="+KEY)
+        locInfo= fetchInfo(ipStackURL+IP+"?access_key="+ipStack_key)
         return locInfo["latitude"]
     except:
         print("no lat")
 
 def getLon(IP):
     try:
-        locInfo= fetchInfo(ipStackURL+IP+"?access_key="+KEY)
+        locInfo= fetchInfo(ipStackURL+IP+"?access_key="+ipStack_key)
         return locInfo["longitude"]
     except:
         print("no lon")
